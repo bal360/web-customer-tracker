e42127dbab8a413a263dc7dda4eec661ac87d05f
Blake A. Long <blake.a.long@gmail.com>
2020-10-09 12:32:56 -0600

    re-factored to add service layer between controller and CustomerDAO

--- src/com/blakelong/springdemo/controller/CustomerController.java
+++ src/com/blakelong/springdemo/controller/CustomerController.java
@@ -8,25 +8,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.blakelong.springdemo.dao.CustomerDAO;
 import com.blakelong.springdemo.entity.Customer;
+import com.blakelong.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// need to inject DAO into controller
-	// spring will scan for a component that implements the interface
-	// @Repository makes DAOImpl findable
+	// refactored to inject CustomerService 
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
+	
 	
 	
 	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		
 		// get customers from DAO
-		List<Customer> customers = customerDAO.getCustomers();
+		List<Customer> customers = customerService.getCustomers();
 		
 		// add the customers to the model
 		model.addAttribute("customers", customers);
---
+++ src/com/blakelong/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.blakelong.springdemo.service;
+
+import java.util.List;
+
+import com.blakelong.springdemo.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+}
