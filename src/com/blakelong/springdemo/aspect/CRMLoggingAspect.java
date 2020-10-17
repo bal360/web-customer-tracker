5df6b840d6da913fb194d614694d875a2f97a3a4
Blake A. Long <blake.a.long@gmail.com>
2020-10-17 10:52:29 -0600

    @Before advice for logging every method call in CustomerController, CustomerService, and CustomerDAO

--- src/com/blakelong/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/blakelong/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,11 @@
 package com.blakelong.springdemo.aspect;
 
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -8,10 +13,35 @@ import org.springframework.stereotype.Component;
 public class CRMLoggingAspect {
 	
 	// set up logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// setup pointcut declarations
+	@Pointcut("execution(* com.blakelong.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.blakelong.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.blakelong.springdemo.dao.*.*(..))")
+	private void forDAOPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage()")
+	private void forAppFlow() {}
 	
 	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint joinPoint) {
+		
+		// display method we are calling
+		String method = joinPoint.getSignature().toShortString();
+		myLogger.info("====> in @Before: calling method: " + method);
+		
+		// display the arguments to the method
+		Object[] args = joinPoint.getArgs();
+		for (Object arg : args) {
+			myLogger.info("====> argument: " + arg);
+		}
+	}
 	
 	// add @AfterReturning advice
 }
